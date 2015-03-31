d0443f577221a91e62da5fd40b5b0c450607e4cb
Robert Elliot <rob@lidalia.org.uk>
2015-03-31 12:57:54 +0100

    Slight cleanup of Age header field name.

--- src/main/scala/uk/org/lidalia/http/core/headerfields/Age.scala
+++ src/main/scala/uk/org/lidalia/http/core/headerfields/Age.scala
@@ -1,17 +1,16 @@
 package uk.org.lidalia.http.core.headerfields
 
-import uk.org.lidalia.http.core.{HeaderField, HeaderFieldName}
+import uk.org.lidalia.http.core.HeaderField
 import org.joda.time.Duration
-import scala.util.control.Exception.catching
 import scala.util.Try
 
 object Age extends SingleValueHeaderFieldName[Duration] {
 
   def apply(duration: Duration) = new Age(duration)
 
-  def name: String = "Age"
+  override def name = "Age"
 
-  def parse(headerFieldValue: String) = Try(Duration.standardSeconds(headerFieldValue.toLong)).toOption
+  override def parse(headerFieldValue: String) = Try(Duration.standardSeconds(headerFieldValue.toLong)).toOption
 }
 
 class Age private (duration: Duration) extends HeaderField(Age.name, List(duration.toStandardSeconds.toString))
