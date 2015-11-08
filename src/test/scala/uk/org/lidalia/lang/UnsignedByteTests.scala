41c3e4c504bd9dcd69f1c87507ed3f7fcf3f5a17
Robert <rob@lidalia.org.uk>
2015-11-08 00:32:39 +0000

    Fixes incorrectly name parameter in test

--- src/test/scala/uk/org/lidalia/lang/UnsignedByteTests.scala
+++ src/test/scala/uk/org/lidalia/lang/UnsignedByteTests.scala
@@ -8,37 +8,37 @@ class UnsignedByteTests extends FunSuite with TableDrivenPropertyChecks {
   test("byte conversion") {
 
     val values = Table(
-      ("String value", "Int value", "Unsigned byte value"),
-      (           "0",           0,         Byte.MinValue),
-      (         "128",         128,              0.toByte),
-      (         "255",         255,         Byte.MaxValue)
+      ("String value", "Int value", "Signed byte value"),
+      (           "0",           0,       Byte.MinValue),
+      (         "128",         128,            0.toByte),
+      (         "255",         255,       Byte.MaxValue)
     )
 
-    forAll(values) { (stringVal, intVal, unsignedByteVal) =>
+    forAll(values) { (stringVal, intVal, signedByteVal) =>
 
-      assert(UnsignedByte(unsignedByteVal).toString == stringVal)
+      assert(UnsignedByte(signedByteVal).toString == stringVal)
       assert(UnsignedByte(stringVal).toString == stringVal)
       assert(UnsignedByte(intVal).toString == stringVal)
 
-      assert(UnsignedByte(unsignedByteVal).toInt == intVal)
+      assert(UnsignedByte(signedByteVal).toInt == intVal)
       assert(UnsignedByte(stringVal).toInt == intVal)
       assert(UnsignedByte(intVal).toInt == intVal)
 
-      assert(UnsignedByte(unsignedByteVal).toSignedByte == unsignedByteVal)
-      assert(UnsignedByte(stringVal).toSignedByte == unsignedByteVal)
-      assert(UnsignedByte(intVal).toSignedByte == unsignedByteVal)
+      assert(UnsignedByte(signedByteVal).toSignedByte == signedByteVal)
+      assert(UnsignedByte(stringVal).toSignedByte == signedByteVal)
+      assert(UnsignedByte(intVal).toSignedByte == signedByteVal)
 
-      assert(UnsignedByte(unsignedByteVal) == UnsignedByte(stringVal))
-      assert(UnsignedByte(unsignedByteVal) == UnsignedByte(intVal))
-      assert(UnsignedByte(unsignedByteVal) == UnsignedByte(unsignedByteVal))
+      assert(UnsignedByte(signedByteVal) == UnsignedByte(stringVal))
+      assert(UnsignedByte(signedByteVal) == UnsignedByte(intVal))
+      assert(UnsignedByte(signedByteVal) == UnsignedByte(signedByteVal))
 
       assert(UnsignedByte(stringVal) == UnsignedByte(stringVal))
       assert(UnsignedByte(stringVal) == UnsignedByte(intVal))
-      assert(UnsignedByte(stringVal) == UnsignedByte(unsignedByteVal))
+      assert(UnsignedByte(stringVal) == UnsignedByte(signedByteVal))
 
       assert(UnsignedByte(intVal) == UnsignedByte(stringVal))
       assert(UnsignedByte(intVal) == UnsignedByte(intVal))
-      assert(UnsignedByte(intVal) == UnsignedByte(unsignedByteVal))
+      assert(UnsignedByte(intVal) == UnsignedByte(signedByteVal))
     }
   }
 }
